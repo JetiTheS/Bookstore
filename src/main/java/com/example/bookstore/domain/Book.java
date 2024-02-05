f53b3575e593e9b19ee02f364ef257358718f665
JetiTheS <jetro.erakangas@myy.haaga-helia.fi>
2024-02-05 13:20:03 +0200

    eka kommit

---
+++ src/main/java/com/example/bookstore/domain/Book.java
@@ -0,0 +1,59 @@
+package com.example.bookstore.domain;
+
+public class Book {
+    private long id;
+    private String title;
+    private String author;
+    private String publicationYear;
+    private String isbn;
+    private String price;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(String publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+}
