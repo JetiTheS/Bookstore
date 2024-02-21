16a0290155061dd58369913d6b24d66cdd88c818
JetiTheS <jetro.erakangas@myy.haaga-helia.fi>
2024-02-21 11:51:21 +0200

    Kirjan lisäys ja poisto lisätty

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.bookstore.domain.Book;
 import com.example.bookstore.domain.BookRepository;
 
 @Controller
@@ -14,9 +18,27 @@ public class BookController {
     private BookRepository repository;
 
     @RequestMapping(value = { "/", "/index" })
-    public String studentList(Model model) {
+    public String bookList(Model model) {
         model.addAttribute("books", repository.findAll());
         return "index";
+    }
+
+    @RequestMapping(value = "/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String saveBook(Book book) {
+        repository.save(book);
+        return "redirect:index";
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+        repository.deleteById(bookId);
+        return "redirect:../index";
     }
 
 }
