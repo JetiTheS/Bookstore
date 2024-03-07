eb50bd919af00fc80342572ea98965d0ca3d1669
JetiTheS <jetro.erakangas@myy.haaga-helia.fi>
2024-03-07 12:36:57 +0200

    Lisäsin Data REST toiminnallisuuden

--- src/main/java/com/example/bookstore/domain/BookRepository.java
+++ src/main/java/com/example/bookstore/domain/BookRepository.java
@@ -3,7 +3,8 @@ package com.example.bookstore.domain;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
-    List<Book> findByTitle(String title);
+    List<Book> findByTitle(@Param("title") String title);
 }
