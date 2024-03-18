ddf7b81fbc64109b8d0d0644bc09b0c1f65ff6ad
JetiTheS <jetro.erakangas@myy.haaga-helia.fi>
2024-03-18 10:48:27 +0200

    Lisäsin eston kirjan poistolle jos ei admin

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -60,6 +60,7 @@ public class BookController {
     }
 
     @RequestMapping(value = "/deletebook/{id}", method = RequestMethod.GET)
+    @PreAuthorize("hasRole('ADMIN')")
     public String deleteBook(@PathVariable("id") Long bookId, Model model) {
         bookrepository.deleteById(bookId);
         return "redirect:../index";
